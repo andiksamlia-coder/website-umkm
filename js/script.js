18becfd65eba190cae45220891a36f647586c058
andiksamlia-coder <andiksamlia@gmail.com>
2026-09-12 11:34:12 +0700

    feat: tambah deskripsi dan foto produk

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-const promoButton = document.querySelector ("#promoButton");
+const promoButton = document.getElementById("promoButton");
+const promoText = document.getElementById("promoText");
 
-promoButton.addEventListener("click",()=> {
-    promoButton.textContent = "promo: beli 2 gratis tester";
-    console.log("promo kopi kenangan berhasil ditampilkan.");
+promoButton.addEventListener("click", function () {
+    promoText.classList.toggle("hidden");
+
+    if (promoButton.textContent === "Lihat Promo Galau Hari Ini") {
+        promoButton.textContent = "Promo Hari Ini";
+    } else {
+        promoButton.textContent = "Lihat Promo Galau Hari Ini";
+    }
 });
