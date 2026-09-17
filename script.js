42a1b3962c1a15601ce81f786525f7a843807fed
andiksamlia-coder <andiksamlia@gmail.com>
2026-09-17 21:25:27 +0700

    feat: informasi kontak UMKM dan menambah link melalui navigasi

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-const promoButton = document.querySelector ("#promoButton");
-const promoButton = document.getElementById("promoButton");
-const promoText = document.getElementById("promoText");
+const promoButton = document.querySelector("#promoButton");
 
-promoButton.addEventListener("click",()=> {
-    promoButton.textContent = "promo: beli 2 gratis tester";
-    console.log("promo kopi kenangan berhasil ditampilkan.");
-promoButton.addEventListener("click", function () {
-    promoText.classList.toggle("hidden");
-
-    if (promoButton.textContent === "Lihat Promo Galau Hari Ini") {
-        promoButton.textContent = "Promo Hari Ini";
-    } else {
-        promoButton.textContent = "Lihat Promo Galau Hari Ini";
-    }
+promoButton.addEventListener("click", () => {
+    promoButton.textContent = "Beli 1 gratis 1!";
+    console.log("Prommo Kopi Nusa berhasil ditampilkan.");
 });
