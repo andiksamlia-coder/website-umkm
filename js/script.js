045470291426d160e734d95f596fb2a363ee8aad
andiksamlia-coder <andiksamlia@gmail.com>
2026-09-21 09:52:36 +0700

    feat P03: Penambahan navigasi dan perbaikan media

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const promoButton = document.querySelector("#promoButton");
 
 promoButton.addEventListener("click", () => {
-  promoButton.textContent = "Promo: Beli 2 gratis tester!";
-  console.log("Promo Kopi Nusa berhasil ditampilkan.");
+    promoButton.textContent = "Beli 1 gratis 1!";
+    console.log("Prommo Kopi Nusa berhasil ditampilkan.");
 });
